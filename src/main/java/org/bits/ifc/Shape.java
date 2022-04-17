16b543f592d3aa45d838f39f61e679448fcd7b69
Onkar Deshpande <onkar.deshpande@oracle.com>
2022-04-17 22:01:41 +0530

    Adding 1st version of project

---
+++ src/main/java/org/bits/ifc/Shape.java
@@ -0,0 +1,7 @@
+package org.bits.ifc;
+
+public interface Shape {
+	double area();
+
+	double perimeter();
+}
---
+++ src/main/java/org/bits/impl/Circle.java
@@ -0,0 +1,22 @@
+package org.bits.impl;
+
+import org.bits.ifc.Shape;
+
+public class Circle implements Shape {
+	private double radius;
+
+	public Circle(double radius) {
+		this.radius = radius;
+
+	}
+
+	@Override
+	public double area() {
+		return Math.PI * radius * radius;
+	}
+
+	@Override
+	public double perimeter() {
+		return 2 * Math.PI * radius;
+	}
+}
