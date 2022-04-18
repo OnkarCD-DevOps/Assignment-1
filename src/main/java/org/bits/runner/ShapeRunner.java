f0fa340b0ea3d07439252364873ce339fd25fa78
OnkarDeshpande-p2 <onkar.deshpande.c@gmail.com>
2022-04-18 19:01:08 +0530

    Minor Changes

--- src/main/java/org/bits/runner/ShapeRunner.java
+++ src/main/java/org/bits/runner/ShapeRunner.java
@@ -6,18 +6,19 @@ import org.bits.impl.Rectangle;
 public class ShapeRunner {
 	public static void main(String[] args) {
 		// Rectangle area and parameter
-		double length = 2.0;
+		double length = 5.0;
 		double breadth = 3.0;
 		Rectangle r = new Rectangle(length, breadth);
 
+		System.out.println("------------Shape:Rectangle---------------");
 		System.out.println("Rectangle - Area: " + r.area());
 		System.out.println("Rectangle - perimeter: " + r.perimeter());
 
 		/*
 		 * Adding another test case for rectangle
 		 */
-		Rectangle r2 = new Rectangle(5.0, breadth);
-
+		Rectangle r2 = new Rectangle(5.0, 3.0);
+		System.out.println("------------Shape:Rectangle---------------");
 		System.out.println("Rectangle - Area: " + r2.area());
 		System.out.println("Rectangle - perimeter: " + r2.perimeter());
 
