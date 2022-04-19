908a163990f15bfed55684818ba894b7e35964b7
OnkarDeshpande-p2 <onkar.deshpande.c@gmail.com>
2022-04-19 15:50:35 +0530

    Add feature2

--- src/main/java/org/bits/runner/ShapeRunner.java
+++ src/main/java/org/bits/runner/ShapeRunner.java
@@ -3,6 +3,7 @@ package org.bits.runner;
 import org.bits.ifc.Shape;
 import org.bits.impl.Circle;
 import org.bits.impl.Rectangle;
+import org.bits.impl.RightTriangle;
 
 public class ShapeRunner {
 	public static void main(String[] args) {
@@ -13,8 +14,6 @@ public class ShapeRunner {
 		System.out.println("------------Shape:Rectangle---------------");
 		System.out.println("Rectangle - Area: " + rect1.area());
 		System.out.println("Rectangle - perimeter: " + rect1.perimeter());
-
-	
 
 		/*
 		 * Adding another test case for rectangle
@@ -31,6 +30,10 @@ public class ShapeRunner {
 		Shape c = new Circle(radius);
 		System.out.println("Circle - Area: " + c.area());
 		System.out.println("Circle - perimeter: " + c.perimeter());
+
+		Shape rgtTriangle = new RightTriangle(3, 4);
+		System.out.println("Area of Right Triangle - " + rgtTriangle.area());
+		System.out.println("Perimeter of right triangle - " + rgtTriangle.perimeter());
 
 	}
 }
